4f4614d6dc4f5ce3083f82cd2c5cc662731e66a8
Manuel Cristóbal Escobedo <manuelcristobalescobedo@gmail.com>
2025-10-05 17:22:07 -0300

    Añadí como comentarios los fragmentos de código que usé como referencia

--- src/Componentes/Tareas.tsx
+++ src/Componentes/Tareas.tsx
@@ -1,9 +1,24 @@
+// import { type Product } from "../types/Product";
+// import productos from "../data/productos.json"
+// import { useEffect, useState } from "react";
+
 import { useState, useEffect } from "react"
 import Tarea from "./Tarea"
 import type { Tarea as TareaType } from "../Tipos/Tarea"
 
+// function Home() {
+
+//     const [contador, setContador] = useState(0);
+//     const [hora, setHora] = useState(new Date())
+
 export default function Tareas() {
+
     const [tareas, setTareas] = useState<TareaType[]>([])
+
+    // useEffect(() => {
+    //     console.log("montando componente");
+    //     return () => { console.log("desmontando componente"); }
+    // }, [contador])
 
     useEffect(() => {
         const tareasGuardadas = localStorage.getItem("tareas")
@@ -12,6 +27,13 @@ export default function Tareas() {
             setTareas(tareasArray)
         }
     }, [])
+
+    // listaProductos.map((producto) => {
+    //     return (
+
+    //         <li key={producto.id}> {producto.nombre} - {producto.stock} -  {producto.uuid}</li>
+    //     )
+    // })
 
     return (
         <div>
